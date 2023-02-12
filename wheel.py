07ecaf91f80f0740582901bb14edeea1107849ae
AndreyRomanov <ansromanov@gmail.com>
2023-02-13 02:03:43 +0400

    custom values

--- wheel.py
+++ wheel.py
@@ -9,7 +9,16 @@ def index():
 
 @app.route('/spin', methods=['POST'])
 def spin():
-    spin_options = ['200', '300', '400', '500', 'BANKRUPT', 'LOSE A TURN']
+    spin_options = []
+    value1 = request.form.get('value1')
+    value2 = request.form.get('value2')
+    value3 = request.form.get('value3')
+    if value1:
+        spin_options.append(value1)
+    if value2:
+        spin_options.append(value2)
+    if value3:
+        spin_options.append(value3)
     spin_result = random.choice(spin_options)
     return spin_result
 
