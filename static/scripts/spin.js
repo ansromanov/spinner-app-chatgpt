6d4cd49cbe237718e41cb09cd834535f00626db9
AndreyRomanov <ansromanov@gmail.com>
2023-02-13 02:27:20 +0400

    fix bugs

--- static/scripts/spin.js
+++ static/scripts/spin.js
@@ -1,14 +1,14 @@
 function spin() {
   const xhr = new XMLHttpRequest();
   xhr.open('POST', '/spin', true);
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.onload = function () {
     if (this.status === 200) {
       document.querySelector('#result').innerHTML = this.responseText;
     }
   };
-  xhr.send();
+
+  const inputs = document.querySelectorAll('input[name="value"]');
+  const values = Array.from(inputs).map(input => input.value);
+  xhr.send(JSON.stringify({ values }));
 }
-
-
-
